733be423894d8f516dcf0459bde24d46b154f4cc
gituser-charan <charantejamodiboyina@gmail.com>
2023-09-20 16:05:39 +0530

    settings

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -161,9 +161,9 @@ SIMPLE_JWT = {
 }
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.hostinger.com"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS =True
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'info@manalocalmall.com'
-EMAIL_HOST_PASSWORD = 'Manalocalmall@2021'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'charantejamodiboyina@gmail.com'
+EMAIL_HOST_PASSWORD = 'pdruwazqcceslees'
 EMAIL_USE_SSL = False
